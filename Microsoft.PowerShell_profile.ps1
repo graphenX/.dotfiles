02b1dfcf5a7422eaa8038b1bc51f724e4905f611
graphenX <graphenX@tutanota.com>
2022-07-06 16:11:01 +0200

    Funtions for set/change IP through windows powershell

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,9 +4,53 @@ Set-Alias -Name ssha -Value SSH_KEY_ADD
 
 Import-Module -Name Terminal-Icons
 
-function SSH_KEY_ADD() {
+function ADD_SSH_KEY() {
     if((Get-Service ssh-agent).Status -eq "Running") {
       ssh-add C:\Users\daniel-farina\.ssh\id_ed25519_graphenX
     }
     else{Write-Output "[*] ssh-agent not running !!"}
+}
+function GET_CONN_IF() {
+  Get-NetIPInterface -ConnectionState "Connected"
+}
+
+function SET_IP_ADDR() {
+  Param(
+        [parameter(Mandatory=$true)]
+        [String]
+        $if,
+
+        [parameter(Mandatory=$true)]
+        [String]
+        $ip,
+
+        [parameter(Mandatory=$true)]
+        [String]
+        $msk,
+
+        [parameter(Mandatory=$true)]
+        [String]
+        $gw
+    )
+
+  Set-NetIPInterface -InterfaceIndex $if -Dhcp Disabled
+  New-NetIPAddress -InterfaceIndex $if -IPAddress $ip -PrefixLength $mk -DefaultGateway $gw
+  Set-DnsClientServerAddress -InterfaceIndex $if -ServerAddresses ("192.168.10.1","8.8.8.8")
+}
+function ADD_IP_ADDR() {
+  Param(
+        [parameter(Mandatory=$true)]
+        [String]
+        $if,
+
+        [parameter(Mandatory=$true)]
+        [String]
+        $ip,
+
+        [parameter(Mandatory=$true)]
+        [String]
+        $msk
+    )
+
+  New-NetIPAddress -InterfaceIndex $if -IPAddress $ip -PrefixLength $mk
 }
